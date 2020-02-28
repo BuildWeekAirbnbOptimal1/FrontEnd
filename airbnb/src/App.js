8c5d8e409412db39fc39fc934f71b60f12530b09
bsherwood9 <36565536+bsherwood9@users.noreply.github.com>
2020-02-27 22:19:10 -0700

    Adding a lot of stuff. Working on Authorization and login page.

--- airbnb/src/App.js
+++ airbnb/src/App.js
@@ -1,9 +1,25 @@
 import React from "react";
-import logo from "./logo.svg";
+import { Link, Route, Switch } from "react-router";
+import LoginForm from "./components/LoginForm";
+import Navigation from "./components/Navigation";
+import ProtectedRoute from "./components/ProtectedRoute";
 import "./App.scss";
 
 function App() {
-  return <div className="App">This is where the magic happens.</div>;
+  return (
+    <div className="App">
+      <Navigation />
+      <Switch>
+        <Route exact path="/" render={() => <LoginForm />} />
+        <Route exact path="/about" render={() => <h1>about</h1>} />
+        <ProtectedRoute
+          exact
+          path="/listings"
+          render={() => <h1>Listings</h1>}
+        />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
